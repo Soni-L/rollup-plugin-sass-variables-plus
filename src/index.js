129ad89654ff42a68be58e8b5adae0a67e0efed2
soni <acdsoni@gmail.com>
2021-03-02 20:41:51 +0100

    modified so it parses without camelCase

--- src/index.js
+++ src/index.js
@@ -1,49 +1,56 @@
-var fs = require('fs'),
-  path = require('path'),
+var fs = require("fs"),
+  path = require("path"),
   paths = new Map();
 
 export default function () {
   return {
     resolveId: function (importee, importer) {
-      if (importee.indexOf('sass-variable-loader') >= 0 || importee.indexOf('variables') >= 0) {
-        const importeeArray = importee.split('!');
+      if (
+        importee.indexOf("sass-variable-loader") >= 0 ||
+        importee.indexOf("variables") >= 0
+      ) {
+        const importeeArray = importee.split("!");
         var name = importeeArray[importeeArray.length - 1],
           target = path.resolve(path.dirname(importer), name);
 
-        paths.set(target, name)
-        return target
+        paths.set(target, name);
+        return target;
       }
     },
-    transform (json, id) {
+    transform(json, id) {
       if (!paths.has(id)) {
-        return
+        return;
       }
 
-      var code = fs.readFileSync(id, 'utf-8')
-      var variables = parse(code, parse(code))
+      var code = fs.readFileSync(id, "utf-8");
+      var variables = parse(code, parse(code));
 
       return {
         code: `export default ${JSON.stringify(variables)}`,
-        map: {mappings: ''}
+        map: { mappings: "" },
       };
-    }
-  }
+    },
+  };
 }
 const matchVarDecl = /\$([^:]+):[ ]*([^;]+);?/;
 const matchVarName = /^\$([\s\S]+)$/;
 
-function camelCase (k) {
+function camelCase(k) {
   return k.replace(/-(.)/gi, (_, l) => l.toUpperCase());
 }
-function parse (content, parsed = {}) {
-  return content.split(`\n`).map((d) => {
-    // console.log(matchVarDecl.exec(d));
-    return matchVarDecl.exec(d)
-  }).filter(t => t)
-    .map(([_, vname, value]) => { // eslint-disable-line no-unused-vars
+function parse(content, parsed = {}) {
+  return content
+    .split(`\n`)
+    .map((d) => {
+      // console.log(matchVarDecl.exec(d));
+      return matchVarDecl.exec(d);
+    })
+    .filter((t) => t)
+    .map(([_, vname, value]) => {
+      // eslint-disable-line no-unused-vars
       const m = matchVarName.exec(value);
       return {
-        [camelCase(vname)]: m ? parsed[camelCase(m[1])] || value : value
+        [vname]: m ? parsed[m[1]] || value : value,
       };
     })
     .reduce((ac, v) => ({ ...ac, ...v }), {});
